f6fffd86a3e7a62e459b579dadd36f224c89bb5d
xfile6912 <xfile6912@naver.com>
2019-11-18 20:33:06 +0900

    add createaccount function and checkaccount function

--- bank.cpp
+++ bank.cpp
@@ -11,25 +11,65 @@
 #include <string>
 #include <vector>
 
+using namespace std;
+
 
 typedef struct acc {
 	int account;
-	int id;
 	int bank;
 	int password;
 	int money;
 }ACC;
 
-void accountcheck()
+vector<ACC> account;
+
+int accountcheck(int tempaccount) //존재하는 계좌이면 1, 존재하지 않는 계좌이면 0;
 {
+	int i;
+	int flag = 0;
+	for (i = 0; i < account.size(); i++)
+	{
+		if (account[i].account == tempaccount)
+			flag = 1;
+	}
+	return flag;
 }
 
-void passwordcheck()
+int passwordcheck() //패스워드가 맞으면 1, 패스워드가 틀리면 0;
 {
+	return 1;
 }
 
 void createaccount()
 {
+	int flag;//존재하는 계좌이면 1, 존재하지 않는 계좌이면 0;
+	ACC tempaccount;//시도하는 어카운트;
+	int password;
+	while (1)
+	{
+		printf("계좌번호를 입력하시오:");
+		scanf("%d", &(tempaccount.account));
+		flag = accountcheck(tempaccount.account);
+		if (flag == 0)
+			break;
+		else
+			printf("이미 존재하는 계좌입니다. 다시 입력하세요!\n");
+	}
+	printf("은행회사를 고르십시오(abank=1; bbank=2; cbank=3):");
+	scanf("%d", &(tempaccount.bank));
+	tempaccount.money = 0;
+	while (1)
+	{
+		printf("패스워드를 입력하십시오 : ");
+		scanf("%d", &(tempaccount.password));
+		printf("다시 한번 입력하십시오 : ");
+		scanf("%d", &password);
+		if (password == tempaccount.password)
+			break;
+		else
+			printf("패스워드가 동일하지 않습니다.\n");
+	}
+	account.push_back(tempaccount);
 }
 void deposit()
 {
